e1154ddc4ad901b4f96380c1af1b07396937912a
antho407 <102033802+antho407@users.noreply.github.com>
2022-03-29 15:42:26 +0300

    Add files via upload

---
+++ product of two integer.c
@@ -0,0 +1,18 @@
+// program that accepts two integers from user and calculate the product
+#include<stdio.h>
+
+int main ()
+{
+int k;
+int h;
+int product ;
+printf("enter the first number\n");
+scanf("%d",&k);
+printf("enter the second number\n");
+scanf("%d",&h);
+product=k*h;
+printf("the product is%d",product);
+scanf("%d",&product);
+
+return 0;
+}
---
+++ sum of five numbers.c
@@ -0,0 +1,16 @@
+// program to print sum of five numbers
+#include<stdio.h>
+
+int main ()
+{
+int k=56;
+int h=34;
+int y=23;
+int f=36;
+int g=100;
+int sum;
+sum=k+h+y+f+g;
+printf("sum of five numbers is%d",sum);
+
+return 0;
+}

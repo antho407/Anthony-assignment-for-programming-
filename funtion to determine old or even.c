dac7934c23595f62f087e14776d1c2fc0784ed27
antho407 <102033802+antho407@users.noreply.github.com>
2022-04-27 09:34:47 +0300

    Add files via upload

---
+++ funtion to determine old or even.c
@@ -0,0 +1,19 @@
+/*a function to determine weather
+a number is even or odd */
+#include <stdio.h>
+int main()
+{
+int number;
+printf("enter the number");
+scanf("%d",&number);
+if(number%2==0)
+{
+printf("even number");
+}
+else
+{
+printf("odd number");
+
+}
+return 0;
+}
